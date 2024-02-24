7f5369abf3da8171c1cc77ea7d2e03d7bb546694
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-02-24 03:53:58 +0000

    Add new fuzz_match function

--- encommon/utils/match.py
+++ encommon/utils/match.py
@@ -7,6 +7,7 @@ is permitted, for more information consult the project license file.
 
 
 
+from fnmatch import fnmatch
 from re import fullmatch as re_fmatch
 from re import match as re_match
 
@@ -61,6 +62,58 @@ def rgxp_match(
         for pattern in patterns:
 
             if not _matches():
+                continue
+
+            matched = True
+
+            break
+
+        outcomes.add(matched)
+
+
+    return bool(
+        outcomes and all(outcomes))
+
+
+
+def fuzz_match(
+    values: str | list[str],
+    patterns: str | list[str],
+) -> bool:
+    """
+    Determine whether or not values are included by patterns.
+
+    Example
+    -------
+    >>> rgxp_match('one', ['one', 'two'])
+    True
+
+    Example
+    -------
+    >>> rgxp_match('uno', ['one', 'two'])
+    False
+
+    :param values: Value or values to enumerate for matching.
+    :param patterns: Patterns which values can match any one.
+    :returns: Boolean indicating outcome from the operation.
+    """
+
+    if isinstance(values, str):
+        values = [values]
+
+    if isinstance(patterns, str):
+        patterns = [patterns]
+
+    outcomes: set[bool] = set()
+
+
+    for value in values:
+
+        matched = False
+
+        for pattern in patterns:
+
+            if not fnmatch(value, pattern):
                 continue
 
             matched = True
