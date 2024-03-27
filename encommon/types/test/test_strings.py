54c6fec092d7b43019dc45132637bf686a9623a1
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-03-27 03:47:28 +0000

    Add new striplower function (#18)

--- encommon/types/test/test_strings.py
+++ encommon/types/test/test_strings.py
@@ -7,15 +7,13 @@ is permitted, for more information consult the project license file.
 
 
 
-from .dicts import merge_dicts
-from .dicts import sort_dict
-from .empty import Empty
-from .strings import striplower
+from ..strings import striplower
 
 
 
-__all__ = [
-    'Empty',
-    'merge_dicts',
-    'sort_dict',
-    'striplower']
+def test_striplower() -> None:
+    """
+    Perform various tests associated with relevant routines.
+    """
+
+    assert striplower('  Foo ') == 'foo'
