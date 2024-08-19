bbe89d0236e457d8e9eec583e69da2119464e8ef
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-08-19 13:04:50 +0000

    Add new colors subpackage

---
+++ encommon/colors/__init__.py
@@ -0,0 +1,14 @@
+"""
+Functions and routines associated with Enasis Network Common Library.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+"""
+
+
+
+from .colors import Color
+
+
+
+__all__ = ['Color']
