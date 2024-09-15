f346acf44b61f70546203f5c3b277ceb1cc336be
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-09-15 11:13:24 +0000

    Add new ensysie functions

--- encommon/types/lists.py
+++ encommon/types/lists.py
@@ -12,6 +12,77 @@ from typing import Sequence
 
 
 
+def dedup_list(
+    value: list[Any],
+    update: bool = True,
+) -> list[Any]:
+    """
+    Return the provided list values with duplicates removed.
+
+    .. warning::
+       This function will update the ``value`` reference.
+
+    :param value: List of values processed for duplication.
+    :returns: Provided list values with duplicates removed.
+    """
+
+    if update is False:
+
+        dedup = dict.fromkeys(value)
+
+        return list(dedup)
+
+
+    unique: list[Any] = []
+
+    for item in list(value):
+
+        if item in unique:
+
+            value.remove(item)
+
+            continue
+
+        unique.append(item)
+
+
+    return value
+
+
+
+def fuzzy_list(
+    values: str | list[str],
+    patterns: str | list[str],
+) -> list[str]:
+    """
+    Return the provided values that match provided patterns.
+
+    :param values: Value or values to enumerate for matching.
+    :param patterns: Patterns which values can match any one.
+    :returns: Provided values that match provided patterns.
+    """
+
+    from ..utils import fuzz_match
+
+    if isinstance(values, str):
+        values = [values]
+
+    matched: list[str] = []
+
+    for value in values:
+
+        match = fuzz_match(
+            value, patterns)
+
+        if match is False:
+            continue
+
+        matched.append(value)
+
+    return matched
+
+
+
 def inlist(
     needle: Any,  # noqa: ANN401
     haystack: Sequence[Any],
